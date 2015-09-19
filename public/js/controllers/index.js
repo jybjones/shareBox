f444af1a95bf4df4facf6ad8d68959e8d0e413fe
Jennifer Jones <jybjones@gmail.com>
2015-09-19 15:36:18 -0500

    added carousel for index

--- public/js/controllers/index.js
+++ public/js/controllers/index.js
@@ -1,3 +1,30 @@
 angular.module('sharebox.system').controller('IndexController', ['$scope', 'Global', function ($scope, Global) {
     $scope.global = Global;
+
+
+    $scope.myInterval = 5000;
+    $scope.noWrapSlides = false;
+    var slides = $scope.slides = [];
+    // var slides = $scope.slides = [
+    //   {
+    //     image: '/img/burEat.gif',
+    //     text: 'Description Of Slide'
+    //   },
+    //   {
+    //     image: '/img/burEat.gif',
+    //     text: 'Description Of Second Slide'
+    //   }
+    // ];
+    $scope.addSlide = function() {
+      var newWidth = 600 + slides.length + 1;
+      slides.push({
+        image: '//placekitten.com/' + newWidth + '/300',
+        text: ['More','Extra','Lots of','Surplus'][slides.length % 4] + ' ' +
+          ['Cats', 'Kittys', 'Felines', 'Cutes'][slides.length % 4]
+      });
+    };
+    for (var i=0; i<4; i++) {
+      $scope.addSlide();
+    }
+    console.log(slides);
 }]);
